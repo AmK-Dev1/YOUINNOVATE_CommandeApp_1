e0a600d9f15793968f0db603456aa71fec1e253e
amkdev <medkhalilallaoui@gmail.com>
2022-03-22 20:40:03 +0100

    last

--- sv4lx7xv/connection.php
+++ sv4lx7xv/connection.php
@@ -2,16 +2,18 @@
 
 
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_NAME','basedesdonnees');
+define('DB_USER','amkdev');
+define('DB_PASSWORD','Khaliliphone7s++');
+define('DB_NAME','CommandeApp');
 
 function connect()
 {
     $connect = mysqli_connect(DB_HOST , DB_USER , DB_PASSWORD , DB_NAME);
+
     if(mysqli_connect_errno($connect)){
         die("failed to connect" . mysqli_connect_error());
     }
+
     return $connect;
 }
 
